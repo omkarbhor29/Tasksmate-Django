0322e0a1aca4be11796b96fcff9525bbc6694edb
omkarbhor29 <omkarbhor2908@gmail.com>
2020-07-22 21:55:34 +0530

    proc file updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-env = environ.Env(SECRET_KEY = str,)
+env = environ.Env(SECRET_KEY=str,)
 environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 
 
